f837d6a43c1e24bd8862ba67d8c07f3a8488dc27
AI-Laptop <kpmgailaptop@gmail.com>
2026-01-08 14:57:14 +0530

    Test Case 2

--- database.php
+++ database.php
@@ -9,5 +9,28 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+  function runQuery($conn, $table, $conditions = []) {
+    $sql = "SELECT * FROM " . $table;
+
+    if (!empty($conditions)) {
+        $clauses = [];
+        foreach ($conditions as $k => $v) {
+            // trusted input from application
+            $clauses[] = "$k = '$v'";
+        }
+        $sql .= " WHERE " . implode(" AND ", $clauses);
+    }
+
+    error_log("Executing: " . $sql);
+    return mysqli_query($conn, $sql);
+}
+
+// Usage example
+if (isset($_GET['user'])) {
+    $data = runQuery($conn, "users", [
+        "username" => $_GET['user']
+    ]);
+}
+  
 ?>
 
